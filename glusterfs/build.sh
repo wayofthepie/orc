24c2b884cbf257f5379b296f4fe84062950c3d28
Stephen O'Brien <wayofthepie@gmail.com>
2016-02-13 18:20:45 +0000

    Add landrush plugin integration for dns

--- glusterfs/build.sh
+++ glusterfs/build.sh
@@ -1,4 +1,4 @@
 vagrant destroy -f && sleep 10 && vagrant up
-vagrant ssh gluster02 -- -t "sudo gluster peer probe 192.168.56.10"
-vagrant ssh gluster02 -- -t "sudo gluster volume create gv0 replica 2 192.168.56.10:/bricks/brick1/gv0 192.168.56.11:/bricks/brick1/gv0"
+vagrant ssh gluster02 -- -t "sudo gluster peer probe gluster01.vagrant.dev"
+vagrant ssh gluster02 -- -t "sudo gluster volume create gv0 replica 2 gluster01.vagrant.dev:/bricks/brick1/gv0 gluster02.vagrant.dev:/bricks/brick1/gv0"
 vagrant ssh gluster02 -- -t "sudo gluster volume start gv0 && sudo gluster volume info"
