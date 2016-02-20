5aae83094a6f9acefd83db1120b96e067acbecfa
Stephen O'Brien <wayofthepie@gmail.com>
2016-02-20 14:40:43 +0000

    Create tls assets on worker nodes

--- os/coreos/tls/gen_assets.sh
+++ os/coreos/tls/gen_assets.sh
@@ -9,12 +9,6 @@ openssl genrsa -out apiserver-key.pem 2048
 openssl req -new -key apiserver-key.pem -out apiserver.csr -subj "/CN=kube-apiserver" -config openssl.cnf
 openssl x509 -req -in apiserver.csr -CA ca.pem -CAkey ca-key.pem -CAcreateserial -out apiserver.pem -days 365 -extensions v3_req -extfile openssl.cnf
 
-# Worker keypairs
-WORKER_FQDN=worker-01.orc.com
-openssl genrsa -out ${WORKER_FQDN}-worker-key.pem 2048
-WORKER_IP=${WORKER_FQDN} openssl req -new -key ${WORKER_FQDN}-worker-key.pem -out ${WORKER_FQDN}-worker.csr -subj "/CN=${WORKER_FQDN}" -config worker-openssl.cnf
-WORKER_IP=${WORKER_FQDN} openssl x509 -req -in ${WORKER_FQDN}-worker.csr -CA ca.pem -CAkey ca-key.pem -CAcreateserial -out ${WORKER_FQDN}-worker.pem -days 365 -extensions v3_req -extfile worker-openssl.cnf
-
 # Cluster admin keypair
 openssl genrsa -out admin-key.pem 2048
 openssl req -new -key admin-key.pem -out admin.csr -subj "/CN=kube-admin"
